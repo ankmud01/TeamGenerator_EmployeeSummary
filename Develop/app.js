36c64969c3b701433d40af0bd0f4659deb1dd4f3
Rebel Soul <mudvari.ankit@gmail.com>
2020-05-05 21:44:14 -0400

    completed adding prompt to create all users and it is functional..

--- Develop/app.js
+++ Develop/app.js
@@ -15,64 +15,85 @@ const managerArr = [];
 const engineerArr = [];
 const internArr = [];
 
-init => {
+function init() {
 
   //TODO - Welcome to Teambuilder App
+  console.log("Please answer following question to build team");
 
-  buildTeam => {
-
-    console.log("Please answer following question to build team");
-
-    function createTeam() {
-      return inquirer.prompt([
-        {
-          type: 'confirm',
-          name: 'createTeam',
-          message: "Do you want to create a team?"
-        }
-      ])
-    };
-  }
+  function createTeam(){
+    inquirer.prompt([
+      {
+        type: 'confirm',
+        name: 'createTeam',
+        message: "Do you want to create a team?"
+      }
+    ]).then(answer =>{
+      console.log("User wants to create a new team? " + answer.createTeam);
+      if(answer.createTeam === true){
+        //TODO
+      return chooseRole();
+      }else{
+        console.log ("Thank You for using TeamGenerator!!!")
+      }
+    })
+  };
 
   function chooseRole() {
-    return inquirer.prompt([
+    inquirer.prompt([
       {
         type: 'list',
         name: 'chooseRole',
         message: 'Which type of team member would you like to add?',
-        choices: ["Manager", "Engineer", "Intern", "I don't want to add any"]
+        choices: ["Manager", "Engineer", "Intern", "I don't want to add any roles"]
       }
-    ])
-  };
+    ]).then(result => {
+      const selectedrole = result.chooseRole
+      switch(selectedrole){
+        case ("Manager"):
+          //TODO - check if we alreaady have manager if not then create manager
+          createManager();
+          break;
+        case("Engineer"):
+          createEngineer();
+          break;
+        case("Intern"):
+          createIntern();
+          break;
+        default:
+          console.log ("Thank You for using TeamGenerator!!!");
+      }
+      //Will need to make this await till user answers other questions
+      //addMoreEmloyees(); 
+    })
+  }
 
-  function createManager() {
+  async function createManager() {
+    console.log("We are creating a manager!!")
     inquirer.prompt([
       {
         type: 'input',
         name: 'name',
-        message: 'What is the managers name?',
-        validate: function (value) {
-          var pass = value.match(
-            /^[a-zA-Z ]{2,30}+$/i
-          );
-          if (pass) {
-            return true;
-          }
-          return 'Please enter a valid name';
-        }
+        message: 'What is the managers name?'
+        // validate: function(value){
+        //   const pass = value.match(/^[a-zA-Z ]{2,30}+$/i);
+        //   if (pass) {
+        //     return true;
+        //   }else{
+        //     return 'Please enter a valid name';
+        //   }
+        // }
       },
       {
         type: 'input',
         name: 'id',
         message: 'What is the managers id?',
         validate: function (value) {
-          var pass = value.match(
-            /^[a-z0-9]+$/i
-          );
+          const pass = value.match(/^[1-9]\d*$/);
           if (pass) {
             return true;
+          } else {
+            return 'Please enter a positive number greater than zero.';
           }
-          return 'Please enter a valid id';
         }
       },
       {
@@ -80,33 +101,31 @@ init => {
         name: 'email',
         message: 'What is the managers email-id?',
         validate: function (value) {
-          var pass = value.match(
-            /\S+@\S+\.\S+$/i
-          );
+          const pass = value.match(/\S+@\S+\.\S+$/i);
           if (pass) {
             return true;
           }
           return 'Please enter a valid email address';
         }
+        
       },
       {
         type: 'input',
         name: 'officeNumber',
         message: 'What is the managers office number?',
         validate: function (value) {
-          var pass = value.match(
-            /^[0-9]$/
-          );
+          const pass = value.match(/^[0-9]$/);
           if (pass) {
             return true;
           }
           return 'Please enter a valid number';
         }
       }
-    ]).then(answer => {
-      const{name,id,email,officeNumber} = response.data
+    ]).then(function (result){
+      const { name, id, email, officeNumber } = result
       const managerUser = new Manager(name, id, email, officeNumber)
       managerArr.push(managerUser);
+      console.log(managerArr);
     })
   };
 
@@ -115,29 +134,28 @@ init => {
         {
           type: 'input',
           name: 'name',
-          message: 'What is the engineers name?',
-          validate: function (value) {
-            var pass = value.match(
-              /^[a-zA-Z ]{2,30}+$/i
-            );
-            if (pass) {
-              return true;
-            }
-            return 'Please enter a valid name';
-          }
+          message: 'What is the engineers name?'
+          // validate: function (value) {
+          //   var pass = value.match(
+          //     /^[a-zA-Z ]{2,30}+$/i
+          //   );
+          //   if (pass) {
+          //     return true;
+          //   }
+          //   return 'Please enter a valid name';
+          // }
         },
         {
           type: 'input',
           name: 'id',
           message: 'What is the engineers id?',
           validate: function (value) {
-            var pass = value.match(
-              /^[a-z0-9]+$/i
-            );
+            var pass = value.match(/^[a-z0-9]+$/i);
             if (pass) {
               return true;
+            }else{
+              return 'Please enter a valid id';
             }
-            return 'Please enter a valid id';
           }
         },
         {
@@ -145,13 +163,12 @@ init => {
           name: 'email',
           message: 'What is the engineers email-id?',
           validate: function (value) {
-            var pass = value.match(
-              /\S+@\S+\.\S+$/i
-            );
+            var pass = value.match(/\S+@\S+\.\S+$/i);
             if (pass) {
               return true;
+            }else{
+              return 'Please enter a valid email address';
             }
-            return 'Please enter a valid email address';
           }
         },
         {
@@ -159,19 +176,19 @@ init => {
           name: 'github',
           message: 'What is the engineers Github Username.',
           validate: function (value) {
-            let pass = value.match(
-              /^[0-9A-Za-z\s\-]+$/i
-            );
+            let pass = value.match(/^[0-9A-Za-z\s\-]+$/i);
             if (pass) {
               return true;
+            }else{
+              return 'Please enter a valid github username';
             }
-            return 'Please enter a valid github username';
           }
         }
-    ]).then(answer => {
-      const{name,id,email,github} = response.data
+    ]).then(function (result){
+      const{name,id,email,github} = result
       const engineerUser = new Engineer(name, id, email, github)
       engineerArr.push(engineerUser);
+      console.log(engineerArr);
     })
   };
 
@@ -182,13 +199,12 @@ init => {
         name: 'name',
         message: 'What is the interns name?',
         validate: function (value) {
-          var pass = value.match(
-            /^[a-zA-Z ]{2,30}+$/i
-          );
+          var pass = value.match(/^[a-zA-Z ]{2,30}+$/i);
           if (pass) {
             return true;
+          }else{
+            return 'Please enter a valid name';
           }
-          return 'Please enter a valid name';
         }
       },
       {
@@ -196,13 +212,12 @@ init => {
         name: 'id',
         message: 'What is the interns id?',
         validate: function (value) {
-          var pass = value.match(
-            /^[a-z0-9]+$/i
-          );
+          var pass = value.match(/^[a-z0-9]+$/i);
           if (pass) {
             return true;
+          }else{
+            return 'Please enter a valid id';
           }
-          return 'Please enter a valid id';
         }
       },
       {
@@ -210,13 +225,12 @@ init => {
         name: 'email',
         message: 'What is the interns email-id?',
         validate: function (value) {
-          var pass = value.match(
-            /\S+@\S+\.\S+$/i
-          );
+          var pass = value.match(/\S+@\S+\.\S+$/i);
           if (pass) {
             return true;
+          }else{
+            return 'Please enter a valid email address';
           }
-          return 'Please enter a valid email address';
         }
       },
       {
@@ -224,24 +238,67 @@ init => {
         name: 'school',
         message: 'What is the interns school name?',
         validate: function (value) {
-          var pass = value.match(
-            /^[a-zA-Z ]{2,30}+$/i
-          );
+          var pass = value.match(/^[a-zA-Z ]{2,30}+$/i);
           if (pass) {
             return true;
+          }else{
+            return 'Please enter a valid school name';
           }
-          return 'Please enter a valid school name';
         }
       }
-    ]).then(answer => {
-      const{name,id,email,school} = response.data
+    ]).then(function(result){
+      const{name,id,email,school} = result
       const internUser = new Intern(name, id, email, school)
       internArr.push(internUser);
+      console.log(internArr);
     })
   };
 
+  async function addMoreEmloyees(){
+    return inquirer.prompt([
+      {
+        type: 'confirm',
+        name: 'addTeamMember',
+        message: "Do you want to add another a team member?"
+      }
+    ]).then((answer) => {
+      console.log("User wants to add new team member? " + answer.addTeamMember);
+      if(answer.addTeamMember === true){
+        buildteam();
+      }else{
+        console.log ("Thank You for using TeamGenerator!!!")
+      }
+    })
+  }
 
+  // async function buildteam() {
+  //   const role = await chooseRole()
+  //     .then((role) => {
+  //       console.log("So the user wants to create a - " + role);
+  //       if (role === "Manager") {
+  //         //TODO
+  //         return createManager();
+  //       }
+  //       else if (role === "Engineer") {
+  //         //TODO
+  //         return createEngineer();
+  //       }
+  //       else if (role === "Intern") {
+  //         //TODO
+  //         return createIntern();
+  //       }
+  //       else {
+  //         return console.log("Thank You for using TeamGenerator!!!")
+  //       }
+  //       //Will need to make this await till user answers other questions
+  //       //addMoreEmloyees(); 
+  //     })
+  // }
+
+
+  createTeam();
 };
+init();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
